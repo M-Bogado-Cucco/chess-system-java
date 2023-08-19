5c917147a02da32d4e75e3c5e8af80c187dcd2e2
Marcelo Bogado <celombc@gmail.com>
2023-08-19 17:57:51 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -40,6 +40,12 @@ public class Board {
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
 	}
+	
+	public void placePiece(Piece piece, Position position) {
+// atribui a peça a matriz na posição dada		
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
 }
 
 
